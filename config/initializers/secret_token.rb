f09156d08d70a84555aca4fa3c12bc91c54edc54
Christopher Cederskog <cc@Nina.local>
2012-06-05 21:02:49 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TabloaderApp::Application.config.secret_token = '20752ac675e78a11e9ed05e64ddc30d08bd1d9aa89dc1275e38638b7c072be0925d69b265e321a7b1d6eb1807e6a67ceac4b1605ddace60fbe1c899bcdba316f'
